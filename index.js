a3a1b62dca6454cbd322459ef6046f015e9290d4
raaynaldo <raaynaldo@hotmail.com>
2020-12-05 14:44:32 -0500

    commit tree e31f3d72cb23c0a92cd7fa62a5385bcc51a12407

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, "");
+  }
+
+  static titleize(string) {
+    let exceptions = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    let arrOfWord = string.split(" ");
+    arrOfWord.forEach((word, idx) => {
+      if (!exceptions.includes(word) || idx == 0) {
+        arrOfWord[idx] = this.capitalize(word);
+      }
+    });
+    return arrOfWord.join(" ");
+  }
 }
